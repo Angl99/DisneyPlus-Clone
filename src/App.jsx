8fa9aa5eb40898369d932804110504494ac104e6
Angl99 <anggelplasencia@pursuit.org>
2024-03-14 21:56:46 -0400

    working on styling more of the landing page and creating a reusable header

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom'
 import Login from './components/Login'
+import Header from './components/Header'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div className='App'>
       <Router>
         <nav>
+          <Header />
           <Link to='/'> </Link>
         </nav>
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,8 +5,19 @@ export default function Login(props) {
         <Container>
             <Content>
                 <CTA>
+                    <FlavorTitle>Stream more of what you love 🚀</FlavorTitle>
                     <CTALogoOne src="src/assets/images/cta-logo-one.svg" />
+                    <Pricing> Starting at <Price>$9.99</Price>/month </Pricing>
                     <SignUp> GET IT NOW </SignUp>
+                    <Description>
+                    Choose a Disney Plan to get started: With all the best Disney+ and Hulu titles, including epic sports from ESPN+
+                    </Description>
+                    <CTALogoTwo src='src\assets\images\cta-logo-two.png'/>
+                    <Footer>
+                    U.S. residents, 18+ only. Offer valid for eligible subscribers only. Select Hulu content available via Disney+ with valid Hulu and Disney+ subscriptions; additional content only available via Hulu app. © 2024 Disney and its related entities. Additional terms apply.
+
+                    All of these and more now streaming on Disney+, Hulu, and ESPN+ with the Disney Bundle.
+                    </Footer>
                 </CTA>
                 <BgImage />
             </Content>
@@ -63,6 +74,26 @@ const CTALogoOne = styled.img`
     width: 100%;
 `;
 
+const CTALogoTwo = styled.img`
+    max-width: 600px;
+    margin-bottom: 20px;
+    display: inline-block;
+    vertical-align: bottom;
+`;
+
+const Pricing = styled.span`
+    color: grey;
+    display: inline;
+    font-size: 18px;
+    padding: 0 0 20px;
+`;
+const Price = styled.span`
+    color: #f9f9f9;
+    display: inline;
+    font-size: 18px;
+    padding: 0 0 20px;
+`;
+
 const SignUp = styled.a`
     font-weight: bold;
     color: #f9f9f9;
@@ -75,7 +106,25 @@ const SignUp = styled.a`
     border: 1px solid transparent;
     border-radius: 4px;
 
-    a:hover {
+    &:hover {
         background-color: #0483ee;
     }
+`;
+
+const Description = styled.p`
+    color: hsla(0, 0%, 95.3%, 1);
+    font-size: 15px;
+    font-weight: bold;
+    margin: 0 0 24px;
+    line-height: 1.5;
+    letter-spacing: .5px;
+`;
+
+const FlavorTitle = styled.h2`
+    color: #f9f9f9;
+`;
+
+const Footer = styled.p`
+    color: grey;
+    font-size: 11px;
 `;
